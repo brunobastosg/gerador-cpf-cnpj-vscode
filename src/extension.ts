36a3444a4266786cebbd264f368f2e9b9aa31f24
Marcos Lacerda <marcos.lacerda@serpro.gov.br>
2021-03-01 15:05:40 -0300

    possibilidade de usar multi cursores

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,38 @@
 'use strict';
 
-import { inserirOuSubstituirTexto } from './editor-utils';
 import { gerarCPF, gerarCNPJ } from './gerador';
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    const somenteNumeros = vscode.workspace.getConfiguration('geradorCpfCnpj').get<boolean>('somenteNumeros');
+    const somenteNumeros = !!vscode.workspace.getConfiguration('geradorCpfCnpj').get<boolean>('somenteNumeros');
 
     const gerarCPFDisposable = vscode.commands.registerCommand('extension.gerarCPF', () => {
-        inserirOuSubstituirTexto(gerarCPF(somenteNumeros));
+        generate(somenteNumeros, gerarCPF);
     });
 
     const gerarCNPJDisposable = vscode.commands.registerCommand('extension.gerarCNPJ', () => {
-        inserirOuSubstituirTexto(gerarCNPJ(somenteNumeros));
+        generate(somenteNumeros, gerarCNPJ);
     });
 
     context.subscriptions.push(gerarCPFDisposable, gerarCNPJDisposable);
+}
+
+function generate(somenteNumeros: boolean, gerador: (somenteNumeros: boolean) => string) {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        console.warn('Nenhum editor ativo.');
+        return;
+    }
+
+    editor.edit((editBuilder) => {
+        editor.selections.forEach( (element) => {
+            editBuilder.replace(
+                element, gerador(somenteNumeros)
+            );
+        });
+    });
+
 }
 
 export function deactivate() {
